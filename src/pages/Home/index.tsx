b16f2e9e6b9a96ff47beca5236d10df0b149a54d
Mustafaev.Ubaydullo <78743719+mustafaevv@users.noreply.github.com>
2024-02-26 14:14:14 +0500

    libries added

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from "styled-components";
+
+const Section = styled.section`
+  background: red;
+`
+
+const Home = () => {
+  return (
+    <Section>home</Section>
+  )
+}
+
+export default Home
